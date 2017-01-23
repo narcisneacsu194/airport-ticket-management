0baf6a4b2deb657f94732eb186e8c799909232e4
Narcis Neacsu <narcisneacsu194@gmail.com>
2017-01-23 12:49:07 +0200

    Added the starter files for the project.

---
+++ src/main/java/com/teamtreehouse/airport/web/controller/FirstController.java
@@ -0,0 +1,14 @@
+package com.teamtreehouse.airport.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
